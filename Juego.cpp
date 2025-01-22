bcfa8269214ba3d353b0af87bdaae38e41cf7b82
alanix2 <alaniznehuen7@gmail.com>
2025-01-22 18:31:47 -0300

    COSAS

--- Juego.cpp
+++ Juego.cpp
@@ -1,8 +1,12 @@
 #include "Juego.h"
 #include <SFML/Window/Event.hpp>
+#include <fstream>
+#include <iostream>
+using namespace std;
 using namespace sf;
 
 Juego::Juego () : m_win(VideoMode(640,480),"Ejemplo de SFML", Style::Titlebar | Style::Close) {
+	leerHighscore();
 	m_win.setFramerateLimit(60);
 	//tendriamos que hacer que el highscore se lea desde un archivo, que tenga los 10 mejores puntajes y 3 letras
 }
@@ -51,14 +55,46 @@ void Juego::Salir ( ) {
 }
 
 int Juego::VerHighscore ( ) const {
-	return m_highscore;
+	return highscore;
+}
+
+void Juego::leerHighscore ( ) {
+	ifstream inFile(filename, std::ios::binary);
+	if (inFile) {
+		inFile.read(reinterpret_cast<char*>(&highscore), sizeof(highscore));
+		if (!inFile) {
+			cerr << "Error leyendo el highscore del archivo.\n";
+			highscore = 0;
+		}
+	} else {
+		// Si el archivo no existe, crear uno con highscore de 0
+		highscore = 0;
+		guardarHighscore();
+	}
+	inFile.close();
+}
+
+void Juego::guardarHighscore ( ) {
+	std::ofstream outFile(filename, std::ios::binary);
+	if (outFile) {
+		outFile.write(reinterpret_cast<const char*>(&highscore), sizeof(highscore));
+		if (!outFile) {
+			std::cerr << "Error escribiendo el highscore en el archivo.\n";
+		}
+	}
+	outFile.close();
 }
 
 void Juego::ActualizarScore (int nuevo_score) {
 	//Aca se tendria que guardar en un archivo
-	if (nuevo_score>m_highscore) m_highscore = nuevo_score;
+	if (nuevo_score>highscore){
+		highscore = nuevo_score;
+		guardarHighscore();
+	}
 }
 
 Juego::~Juego ( ) {
 	delete m_EscenaActual;
 }
+
+
--- Juego.h
+++ Juego.h
@@ -2,29 +2,32 @@
 #define JUEGO_H
 #include "Escena.h"
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <string>
+using namespace std;
 using namespace sf;
 
 class Juego {
 public:
 	Juego();
 	void Jugar(Escena *primera_escena);
-	void ProcesarEventos();
-	void Dibujar();
 	void cambiarEscena(Escena *nueva_escena);
 	void Salir();
 	int VerHighscore() const;
-	void Actualizar();
 	void ActualizarScore(int nuevo_score);
 	~Juego();
 	
 private:
+	void Actualizar();
+	void Dibujar();
+	void ProcesarEventos();
+	void leerHighscore();
+	void guardarHighscore();
 	RenderWindow m_win;
 	Escena *m_EscenaActual = nullptr, *m_ProximaEscena = nullptr; //Escena actual y proxima escena
 	
-	//mayor puntaje, esto se tendria que guardar y leer desde un archivo y poder mostrar los mejores puntajes.
-	//podria ser, un vector de 10 enteros o strings con los 10 mejores puntajes y 3 letras
-	//ejemplo: NHE 1865323
-	int m_highscore = 0;
+	//luego habria que poner los 10 mayores puntajes junto a las primeras 3 iniciales.
+	int highscore = 0;
+	string filename = "savedata.bin";
 };
 
 #endif
